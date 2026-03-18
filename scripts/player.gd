6296f9f3c97d50f5e5ee5d1d67dcf13c3e307a30
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2026-03-18 14:03:36 +0300

    Refined movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,9 @@
 class_name Player extends CharacterBody2D
 
-@export var jump_acceleration: float = 3333 + 5000
-@export var gravity: float = 5000
-@export var max_jump_speed: float = 1666
-@export var max_fall_speed: float = 1000
+@export var gravity: float = 4411.7
+@export var jump_acceleration: float = 3333.3 + gravity
+@export var max_jump_speed: float = 1666.5
+@export var max_fall_speed: float = 882.4
 
 
 func _physics_process(delta: float) -> void:
@@ -18,8 +18,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("jump"):
 		if Input.is_action_just_pressed("jump") and velocity.y > 0:
 			velocity.y = 0
-
 		velocity.y -= jump_acceleration * delta
+	
+	if Input.is_action_just_released("jump"):
+		if velocity.y < 0:
+			velocity.y *= 0.75
 	
 	velocity.y = clampf(velocity.y, -max_jump_speed, max_fall_speed)
 
