ae8e13a67093e473f3fc720c108436dd6060bde1
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2026-04-07 18:43:33 +0300

    Add zapper base and beam visuals

--- scripts/zapper.gd
+++ scripts/zapper.gd
@@ -3,7 +3,9 @@ class_name Zapper extends Area2D
 @export var zapper_data: ZapperData
 
 @onready var collision: CollisionShape2D = $CollisionShape2D
-@onready var sprite: ColorRect = $ColorRect
+@onready var left_sprite: AnimatedSprite2D = $LeftSprite
+@onready var right_sprite: AnimatedSprite2D = $RightSprite
+@onready var beam: AnimatedSprite2D = $Beam
 
 var length: float = 270
 var width: float = 60
@@ -21,8 +23,12 @@ func apply_zapper_data() -> void:
 	position.x += zapper_data.x_offset
 	position.y = clamp([0, 0, 1, 2, 3, 4, 5].pick_random() * -138 - 80, -zapper_data.max_y, -zapper_data.min_y)
 
-	sprite.size = Vector2(width, length)
-	sprite.position.y = -length / 2
+	left_sprite.position.y = length / 2 - 35
+	right_sprite.position.y = -(length / 2 - 41)
+
+	var tiling: float = (length - 118) / 152
+	(beam.material as ShaderMaterial).set_shader_parameter("tiling", Vector2(tiling, 1))
+	beam.scale.x = 2 * tiling
 
 	var shape := collision.shape as CapsuleShape2D
 	shape.radius = width / 2
